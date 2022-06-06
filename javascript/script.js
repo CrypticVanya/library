0afcc93c37fa4673a1fadf7a7287f8b57495b0b6
Vanya <crypticvanya@gmail.com>
2022-06-05 20:44:59 -0400

    have the user input display on the screen

--- javascript/script.js
+++ javascript/script.js
@@ -1,23 +1,53 @@
-    let myLibrary = []
-    
 
-
-function Book(title, author, pages, read) {
+function Book(title, author, pages) {
     this.title = title
     this.author = author
     this.pages = pages
-    this.read = read
     this.info = function() {
-        return `${title} by ${author}, ${pages}, ${read}` 
+        return `${title} by ${author}, ${pages},` 
     }
 }
 
-function addBookToLibrary(title, author, pages, read) {
-    const newBook = new Book(title, author, pages, read)
+
+
+function Library() {
+    this.book = []
+
+    this.addBook = function(book) {
+    this.book.push(book)
+    }
 }
 
-const theHobbit = new Book('The Hobbit', 'J.R.R. Tolkien', '295 pages', 'not read yet')
 
+const userEntry = new Book('lol', 'lol', 'lol')
+
+const library = new Library() 
+
+library.addBook(userEntry)
+
+
+
+function displayOnScreen() {
+    for(let i = 0; 1 < library.book.length; i++) {
+        const carousel = document.querySelector('.carousel-container')
+        const title = document.createElement('h1')
+        const author = document.createElement('p')
+        const pages = document.createElement('p')
+        let book_title = library.book[i].title
+        let book_author = library.book[i].author
+        let book_pages = library.book[i].pages
+        title.textContent = book_title
+        author.textContent = book_author
+        pages.textContent = book_pages
+        carousel.appendChild(title)
+        carousel.appendChild(author)
+        carousel.appendChild(pages)
+}
+}
+
+
+
+//Code to make an invisible form pop up and ask for the info
 
 function openForm() {
     document.getElementById('popUpForm').style.display = 'block'
@@ -33,42 +63,21 @@ btn.addEventListener('click', () => {
     openForm()
 })
 
-
 const cancelBtn = document.querySelector('.btn-cancel')
 cancelBtn.addEventListener('click', () => {
     closeForm()
 })
 
-
-/*
-
-let slideIndex = 1
-showSlides(slideIndex)
-
-function plusSlides(n) {
-    showSlides(slideIndex += n)
-}
-
-function currentSlide(n) {
-    showSlides(slideIndex = n)
-}
-
-function showSlides(n) {
-    let i
-    let slides = document.getElementsByClassName("book-carousel-item")
-    if (n > slides.length) {
-        slideIndex = 1
-    } if (n < 1) {
-        slideIndex = slides.length
-    } 
-    for (i = 0; i < slides.length; i++) {
-        slides[i].style.display = 'none'
-    }
-    slides[slideIndex-1].style.display = 'block'
-}
-
-*/
-
+const sendBtn = document.querySelector('.lol')
+sendBtn.addEventListener('click', function getTarget(e) {
+    e.preventDefault()
+    const userTitle = document.getElementById('title').value
+    const userAuthor = document.getElementById('author').value
+    const userPages = document.getElementById('pages').value
+    const newBookEntry = new Book(userTitle, userAuthor, userPages)
+    library.addBook(newBookEntry)
+    displayOnScreen()
+})
 
 
 
